cffc29f471f4df85426fd4e162835c5b0725aeea
Thaseenthan <thaseenthan0812@gmail.com>
2026-06-13 12:37:44 +0530

    feat: add constants utility file with navigation, social, and tour links

---
+++ Ocean Paradise Tours/src/utils/constants.js
@@ -0,0 +1,23 @@
+export const NAV_LINKS = [
+  { label: 'Home', path: '/' },
+  { label: 'Tours', path: '/tours' },
+  { label: 'Gallery', path: '/gallery' },
+  { label: 'About', path: '/about' },
+  { label: 'Contact', path: '/contact' },
+  { label: 'Booking', path: '/booking' },
+]
+
+export const SOCIAL_LINKS = [
+  { label: 'Instagram', href: 'https://instagram.com' },
+  { label: 'Facebook', href: 'https://facebook.com' },
+  { label: 'YouTube', href: 'https://youtube.com' },
+]
+
+export const TOUR_OPTIONS_FALLBACK = [
+  'Whale Watching',
+  'Dolphin Watching',
+  'Pigeon Island Snorkeling',
+  'Scuba Diving',
+  'Deep Sea Fishing',
+  'Sunset Boat Tour',
+]
